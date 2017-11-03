b768902a4052cef938e512776f703e6fa17fbffc
Maximilian Witte <maximilian.t.witte@gmail.com>
2017-11-03 17:50:27 -0500

    Update main.js to use interval instead of sleep

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 var fs = require('fs');
-var sleep = require('sleep');
+//var sleep = require('sleep');
 var process = require('child_process')
 
-var SECONDS_PER_REQUEST = 10;
+var SECONDS_PER_REQUEST = 60;
 
-while(1) {
+setInterval(function(){
 	var ls = process.execFile('node', ['index.js']);
-
-	sleep.sleep(SECONDS_PER_REQUEST);
-}
+	console.log(Date.now());
+}, (SECONDS_PER_REQUEST * 1000));
